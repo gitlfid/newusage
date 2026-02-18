97177df8e2314efeeb9b8b3ccddf0c4c7a7b765c
gitlfid <github@linksfield.id>
2026-02-18 10:19:52 +0000

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-staging',
+        'version' => 'dev-staging',
+        'reference' => 'da4aca2d9dbd77588ab007972748d5c451cd3a03',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-staging',
+            'version' => 'dev-staging',
+            'reference' => 'da4aca2d9dbd77588ab007972748d5c451cd3a03',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
